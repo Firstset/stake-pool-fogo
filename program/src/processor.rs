7fb3046608a67c7a4abfd1365d43578c7103e502
Bernat <accounts@bernat.xyz>
2025-10-22 13:38:15 +0200

    2. redundant stake pool checks nit (part 2)

--- program/src/processor.rs
+++ program/src/processor.rs
@@ -3482,11 +3482,6 @@ impl Processor {
         // Check the system program
         check_system_program(system_program_info.key)?;
 
-        // Check the token program is the Pools Token program
-        if stake_pool.token_program_id != *token_program_info.key {
-            return Err(ProgramError::IncorrectProgramId);
-        }
-
         // Check that the WSOL mint is the native mint (So11111111111111111111111111111111111111112)
         if *wsol_mint_info.key != native_mint::id() {
             msg!("WSOL mint: {:?}", wsol_mint_info.key);
